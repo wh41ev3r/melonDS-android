f09c75cc0fdd064e45606fef4eaf4611d3ff4f01
wh41ev3r <liquidfae@proton.me>
2026-04-21 21:57:12 +0800

    Amongus

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,7 +1,7 @@
 object AppConfig {
     const val compileSdkVersion = 36
     const val targetSdkVersion = compileSdkVersion
-    const val minSdkVersion = 24
+    const val minSdkVersion = 28
     const val ndkVersion = "28.0.13004108"
 
     const val versionCode = 39
