53b23ad8b5b7c69b91a65bbed1b102363c52314f
Rafael Caetano <rafaelvcaetano@hotmail.com>
2026-04-27 18:49:31 +0100

    Fix secondary display being detected when certain apps want to record the screen

--- app/src/main/java/me/magnum/melonds/impl/layout/SecondaryDisplaySelector.kt
+++ app/src/main/java/me/magnum/melonds/impl/layout/SecondaryDisplaySelector.kt
@@ -9,11 +9,18 @@ import javax.inject.Inject
 
 class SecondaryDisplaySelector @Inject constructor() {
 
+    private companion object {
+        val ExcludedDisplays = listOf(
+            "HiddenDisplay", // Seems like a placeholder display present in some devices
+            "WebRTC_ScreenCapture", // Used by apps that record the screen (e.g.: Discord)
+        )
+    }
+
     fun getSecondaryDisplay(context: Context): Display? {
         val currentDisplay = ContextCompat.getDisplayOrDefault(context)
         return context.getSystemService<DisplayManager>()?.let { displayManager ->
             displayManager.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION).firstOrNull {
-                it.displayId != currentDisplay.displayId && it.name != "HiddenDisplay"
+                it.displayId != currentDisplay.displayId && it.name !in ExcludedDisplays
             }
         }
     }
